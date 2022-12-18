081b5c264158ed7f0a22645b9cb9a07a3649cdd0
eXp3ct <roma.veselov.1990@mail.ru>
2022-12-19 00:16:12 +0300

    Выполнено Лаба 10 - Задание 43

--- Lab10/Program.cs
+++ Lab10/Program.cs
@@ -6,18 +6,31 @@
         private static int _m = 0;
         public static void Main(string[] args)
         {
+            //ExecuteVariant24();
+            ExecuteVariant43();
+        }
+
+        private static void ExecuteVariant43()
+        {
+            Console.WriteLine("Введите порядок матрицы M: ");
+            _n = int.Parse(Console.ReadLine()); // Вводим размер матрицы
+            Var43 var43 = new(_n); // Создаем экземляр класса 43 и передаем ему размер
+        }
+
+        private static void ExecuteVariant24()
+        {
             Console.WriteLine("Введите размерность матрицы MxN: ");
-            _n = int.Parse(Console.ReadLine());
+            _n = int.Parse(Console.ReadLine()); // Вводим размеры матрицы
             _m = int.Parse(Console.ReadLine());
-            Var24 var24 = new(_n, _m);
+            Var24 var24 = new(_n, _m); // Создаем экземляр класса 24 и передаем ему размер
         }
 
         public static void PrintArray(int[,] array) // Вывод массива
         {
             Console.WriteLine("<-----||----->");
-            for (int i = 0; i < _n; i++)
+            for (int i = 0; i < array.GetLength(0); i++)
             {
-                for (int j = 0; j < _m; j++)
+                for (int j = 0; j < array.GetLength(1); j++)
                 {
                     Console.Write(array[i, j]+ " ");
                 }
@@ -28,9 +41,9 @@
         public static void FillArray(int[,] array) // Заполнение массива с клавиатуры
         {
             Console.WriteLine("Введите элементы массива: ");
-            for (int i = 0; i < _n; i++)
+            for (int i = 0; i < array.GetLength(0); i++)
             {
-                for (int j = 0; j < _m; j++)
+                for (int j = 0; j < array.GetLength(1); j++)
                 {
                     array[i,j] = int.Parse(Console.ReadLine());
                 }
@@ -41,9 +54,9 @@
             if (!random)
                 FillArray(array);
             Random randomValue = new(DateTime.Now.Millisecond);
-            for (int i = 0; i < _n; i++)
+            for (int i = 0; i < array.GetLength(0); i++)
             {
-                for (int j = 0; j < _m; j++)
+                for (int j = 0; j < array.GetLength(1); j++)
                 {
                     array[i,j] = randomValue.Next(1, 10);
                 }
---
+++ Lab10/Var43.cs
@@ -0,0 +1,50 @@
+﻿namespace Lab10
+{
+    internal class Var43
+    {
+        private int[,] _array;
+
+        public Var43(int M)
+        {
+            _array = new int[M, M]; // Создаем массив(матрицу)
+
+            Program.FillArray(_array, true); // Заполняем его
+
+            Program.PrintArray(_array); // Выводим на экран
+
+            List<float> result = DiagonalsAvarage(_array); // Лист средних арифм. диагоналей
+            foreach (var item in result)
+                Console.Write(item + " "); // Выводим все его элементы
+
+
+        }
+        
+        private List<float> Diagonal(int[,] array, int diagonalNum) // Метод возвращающий элементы заданной диагонали
+        {
+            List<float> result = new(); // Лист элементов диагонали
+
+            for (int i = 0; i < array.GetLength(0); i++) // Перебираем все строки
+            {
+                for (int j = 0; j < array.GetLength(1); j++) // Перебираем все столбцы
+                {
+                    if (i + diagonalNum != j) // Если пропускаем итерацию, в которой элементы не находятся на данной диагонали
+                        continue;
+                    result.Add(array[i, j]); // Записываем элемент в лист
+                }
+            }
+
+            return result; // Возвращаем лист
+        }
+        private List<float> DiagonalsAvarage(int[,] array) // Метод для нахождения среднего значения диагоналей
+        {
+            List<float> diagonalsAvarage = new(); // Лист средних значений
+            
+            for (int i = array.GetLength(0) - 1; i >= 0; i--) // Перебираем диагонали, параллельных главной, начиная с правого верхнего элемента
+                diagonalsAvarage.Add(Diagonal(array, i).Average()); // Записываем в лист среднее значение текущей диагонали
+            for (int i = 1; i < array.GetLength(0); i++) // Т.к предыдущий цикл идет то i = 0, т.е до главной диагонали, то следующий цикл будет перебирать диагонали ниже главной
+                diagonalsAvarage.Add(Diagonal(array, -i).Average()); // Записываем в лист среднее значение текущей диагонали
+
+            return diagonalsAvarage; // Возвращаем лист средних значений
+        }
+    }
+}
